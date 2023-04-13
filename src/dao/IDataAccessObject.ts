db403caf51195499b7be0b10b5b7306c1e14b39c
EliaSuyapaCastro <eliacastro402@gmail.com>
2023-04-13 16:22:10 -0600

    seguimiento1

--- src/dao/IDataAccessObject.ts
+++ src/dao/IDataAccessObject.ts
@@ -1,5 +1,4 @@
 export interface IDataAccessObject {
-<<<<<<< HEAD
     findAll: Function;
     findByID: Function;
     create: Function;
@@ -11,16 +10,3 @@ export interface IDataAccessObject {
     getConnection: Function;
     rawUpdate: Function;
   }
-=======
-  findAll: Function;
-  findByID: Function;
-  create: Function;
-  update: Function;
-  delete: Function;
-  findByFilter: Function;
-  findOneByFilter: Function;
-  aggregate: Function;
-  getConnection: Function;
-  rawUpdate: Function;
-}
->>>>>>> 571d33f54f4dc9a177f146ce7649355076983bd3
--- src/dao/models/Empresas/IEmpresas.ts
+++ src/dao/models/Empresas/IEmpresas.ts
@@ -1,5 +1,4 @@
 export interface IEmpresa {
-<<<<<<< HEAD
     codigo: string;
     nombre: string;
     status: string;
@@ -7,12 +6,3 @@ export interface IEmpresa {
     updated?: Date;
     observacion?: string;
   }
-=======
-  codigo: string;
-  nombre: string;
-  status: string;
-  created?: Date;
-  updated?: Date;
-  observacion?: string;
-}
->>>>>>> 571d33f54f4dc9a177f146ce7649355076983bd3
